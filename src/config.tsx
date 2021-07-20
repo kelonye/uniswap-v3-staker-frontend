e5ac3875a7c6c88f28a7ee73601eba23b443c52d
vbstreetz <vbstreetz@gmail.com>
2021-07-20 22:56:03 +0300

    feat: more mainnet config

--- src/config.tsx
+++ src/config.tsx
@@ -1,11 +1,10 @@
 export const BORDER_RADIUS = 4;
-export const APP_NAME = 'Witswap | Stake';
+export const APP_NAME = 'Witswap | Staking';
 
 export const LG_BREAKPOINT = 'md';
 export const SM_BREAKPOINT = 'sm';
 
 export const IS_DEV = process.env.NODE_ENV === 'development';
-// export const IS_DEV = false;
 
 export const CACHE_WALLET_KEY = 'wallet';
 
@@ -20,18 +19,22 @@ export const EWIT_ADDRESS: Record<string, string> = {
 
 export const USDC_ADDRESS: Record<string, string> = {
   [NETWORK_RINKEBY]: '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b',
-  [NETWORK_MAINNET]: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  [NETWORK_MAINNET]: '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b',
 };
 
 export const NFT_POSITIONS_MANAGER_ADDRESS: Record<string, string> = {
   [NETWORK_RINKEBY]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+  [NETWORK_MAINNET]: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
 };
 
 export const STAKING_REWARDS_ADDRESS: Record<string, string> = {
   [NETWORK_RINKEBY]: '0xc462aB5e66067153Bf1B368493E4744C1cA4BeC9',
+  [NETWORK_MAINNET]: '',
 };
 
 export const SUBGRAPHS: Record<string, string> = {
   [NETWORK_RINKEBY]:
-    'https://api.thegraph.com/subgraphs/name/vbstreetz/witstap-staking-rinkeby',
+    'https://api.thegraph.com/subgraphs/name/vbstreetz/witwtap-staking-rinkeby',
+  [NETWORK_MAINNET]:
+    'https://api.thegraph.com/subgraphs/name/vbstreetz/witwtap-staking',
 };
